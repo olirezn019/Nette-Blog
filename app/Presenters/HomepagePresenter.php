87fc3adcbe07ea91f52a9d6374a345d057817a78
Oliver Řezníček <u19.423@pslib.cloud>
2021-06-08 09:26:53 +0200

    first commit

---
+++ app/Presenters/HomepagePresenter.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+
+
+final class HomepagePresenter extends Nette\Application\UI\Presenter
+{
+    private Nette\Database\Explorer $database;
+
+	public function __construct(Nette\Database\Explorer $database)
+	{
+		$this->database = $database;
+	}
+
+    public function renderDefault(): void
+    {
+        $this->template->posts = $this->database->table('posts')
+            ->order('created_at DESC')
+            ->limit(5);
+    }
+}
---
+++ app/Presenters/PostPresenter.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+class PostPresenter extends Nette\Application\UI\Presenter
+{
+	private Nette\Database\Explorer $database;
+
+	public function __construct(Nette\Database\Explorer $database)
+	{
+		$this->database = $database;
+	}
+
+    public function renderShow(int $postId): void
+    {
+        $post = $this->database->table('posts')->get($postId);
+        if (!$post) {
+            $this->error('Post not found');
+        }
+
+        $this->template->post = $post;
+        // Add comments into the page
+	    $this->template->comments = $post->related('comments')->order('created_at');
+    }
+
+    protected function createComponentCommentForm(): Form
+    {
+        $form = new Form; // means Nette\Application\UI\Form
+
+        $form->addText('name', 'Your name:')
+            ->setRequired();
+
+        $form->addEmail('email', 'Email:');
+
+        $form->addTextArea('content', 'Comment:')
+            ->setRequired();
+
+        $form->addSubmit('send', 'Publish comment');
+
+        $form->onSuccess[] = [$this, 'commentFormSucceeded'];
+        return $form;
+    }
+
+    public function commentFormSucceeded(\stdClass $values): void
+    {
+        $postId = $this->getParameter('postId');
+
+        $this->database->table('comments')->insert([
+            'post_id' => $postId,
+            'name' => $values->name,
+            'email' => $values->email,
+            'content' => $values->content,
+        ]);
+
+        $this->flashMessage('Thank you for your comment', 'success');
+        $this->redirect('this');
+    }
+
+    protected function createComponentPostForm(): Form
+    {
+        $form = new Form;
+
+        $form->addText('title', "Title:")->setRequired();
+        $form->addTextArea('content', 'Content:')->setRequired();
+        $form->addSubmit('send', "Save and Publish");
+
+        $form->onSuccess[] = [$this, 'postFormSucceeded'];
+
+        return $form;
+    }
+
+    public function postFormSucceeded(array $values): void
+    {
+        $post = $this->database->table('posts')->insert($values);
+
+        $this->flashMessage('Post was published', 'success');
+        $this->redirect('show', $post->id);
+    }
+
+    public function actionEdit(int $postId): void
+    {
+        $post = $this->database->table('posts')->get($postId);
+        if (!$post) {
+            $this->error('Post not found');
+        }
+        $this['postForm']->setDefaults($post->toArray());
+    }
+}
